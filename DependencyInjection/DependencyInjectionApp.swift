32b2591c6bc758f9b954f2a779a794f1528538c1
Christian Skorobogatow <christiansko@icloud.com>
2022-08-12 12:10:43 +0200

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Christian Skorobogatow on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
